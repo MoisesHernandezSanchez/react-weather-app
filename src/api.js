fa6aded534292f22b998a05edd5183066b43c62d
Moises Hernandez Sanchez <telekineticjake@gmail.com>
2022-07-15 15:34:38 -0400

    Created weather app w/ current weather & forecast

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '6ba7ae1232msh4879e1d294aec42p126788jsnb10f4766236e',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/'
+export const WEATHER_API_KEY = '2b75c04adb06246719eddc697d9e28b8'
